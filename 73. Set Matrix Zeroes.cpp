ede4abd054cbcf15920b6174b0479366287cc79e
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-04-08 13:19:27 +0600

    Create 73. Set Matrix Zeroes.cpp
    https://leetcode.com/problems/set-matrix-zeroes/description/

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // column[m] --> matrix[0][...]
+        // row[n] --> matrix[...][0]
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1, i ,j;
+
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+
+                    if(j==0)
+                    {
+                        col0 = 0;
+                    }
+                    else
+                    {
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+        }
+
+        for(i=1;i<n;i++)
+        {
+            for(j=1;j<m;j++)
+            {
+                if(matrix[0][j] == 0 || matrix[i][0]==0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0)
+        {
+            for(j=0;j<m;j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+        if(col0 == 0)
+        {
+            for(i=0;i<n;i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+};
