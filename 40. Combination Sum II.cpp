c7d9a6fda971fae56604479fc32189466fef7e27
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-04-27 16:49:50 +0600

    Create 40. Combination Sum II.cpp
    https://leetcode.com/problems/combination-sum-ii/description/

---
+++ 40. Combination Sum II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void CombinationSum( int idx , int target , vector<int> &arr,vector<int> &ds, vector<vector<int>>&ans)
+    {
+        if(target == 0)
+        {
+            ans.push_back(ds);
+            return;
+        }
+
+        for(int i = idx;i<arr.size();i++)
+        {
+            if(i > idx  && arr[i] == arr[i-1]) continue;
+
+            if(arr[i] > target) break;
+
+            ds.push_back(arr[i]);
+            CombinationSum( i+1 , target- arr[i] , arr, ds, ans);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        CombinationSum( 0 , target , candidates, ds, ans);
+
+        return ans;
+    }
+};
